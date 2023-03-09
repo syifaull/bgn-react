8169e6d0e757211a3d83cd84f2baa09f1d20cb5b
Syifa Ulyanida <syifaulya0610@gmail.com>
2023-03-09 10:20:22 +0700

    add detail

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,8 +4,8 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 const Navigation = () => {
   return (
     <div>
-      <Navbar variant="dark">
-        <Container>
+      <Navbar variant="dark" expand="lg">
+        <Container fluid>
           <Navbar.Brand href="/">
             <img
               src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png"
@@ -13,26 +13,33 @@ const Navigation = () => {
               className="starwars-logo"
             />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/people" className="text-shine">
-              People
-            </Nav.Link>
-            <Nav.Link href="/films" className="text-shine">
-              Films
-            </Nav.Link>
-            <Nav.Link href="/planets" className="text-shine">
-              Planets
-            </Nav.Link>
-            <Nav.Link href="/species" className="text-shine">
-              Species
-            </Nav.Link>
-            <Nav.Link href="/starships" className="text-shine">
-              Starships
-            </Nav.Link>
-            <Nav.Link href="/vehicles" className="text-shine">
-              Vehicles
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/people" className="text-shine">
+                People
+              </Nav.Link>
+              <Nav.Link href="/films" className="text-shine">
+                Films
+              </Nav.Link>
+              <Nav.Link href="/planets" className="text-shine">
+                Planets
+              </Nav.Link>
+              <Nav.Link href="/species" className="text-shine">
+                Species
+              </Nav.Link>
+              <Nav.Link href="/starships" className="text-shine">
+                Starships
+              </Nav.Link>
+              <Nav.Link href="/vehicles" className="text-shine">
+                Vehicles
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
--- src/pages/DetailPeople.jsx
+++ src/pages/DetailPeople.jsx
@@ -1,9 +1,12 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Load from "../components/Load";
 import Navigation from "../components/Navigation";
+import SmallPic from "../components/SmallPic";
 
 const DetailPeople = () => {
   const location = useLocation();
@@ -33,22 +36,83 @@ const DetailPeople = () => {
       <div className="container min-vh-100">
         <Navigation />
         <br></br>
-        <div className="d-flex">
-          <div>
-            <img
-              src="https://w0.peakpx.com/wallpaper/23/577/HD-wallpaper-darth-vader-star-wars-sith-lightsaber-star-wars-battlefront-video-games-portrait-display-simple-background.jpg"
-              alt="detail"
-              className="image-detail"
-            />
-          </div>
-          <div>
-            {isLoading ? (
+        <a href="/people">
+          <FontAwesomeIcon icon={faArrowLeft} size="xl" className="text-gray" />
+        </a>
+        <div className="">
+          <div className="row">
+            <div className="col-md-6 d-flex justify-content-center align-items-center">
               <div>
-                <Load />
+                <img
+                  src="https://w0.peakpx.com/wallpaper/23/577/HD-wallpaper-darth-vader-star-wars-sith-lightsaber-star-wars-battlefront-video-games-portrait-display-simple-background.jpg"
+                  alt="detail"
+                  className="image-detail"
+                />
               </div>
-            ) : (
-              <div className="row">{detail.name}</div>
-            )}
+            </div>
+            <div className="col-md-6 p-4 md:px-0 d-flex align-items-center">
+              {isLoading ? (
+                <div>
+                  <Load />
+                </div>
+              ) : (
+                <div>
+                  <p className="fs-3 fw-semibold">{detail.name}</p>
+                  <p>Height: {detail.height} cm</p>
+                  <p>Mass: {detail.mass} kilos</p>
+                  <p>Hair Color: {detail.hair_color}</p>
+                  <p>Skin Color: {detail.skin_color}</p>
+                  <p>Eye Color: {detail.eye_color}</p>
+                  <p>Birth Year: {detail.birth_year}</p>
+                  <p>Gender: {detail.gender}</p>
+                  <div className="d-flex gap-3">
+                    <div>
+                      <p>
+                        Homeworld: <SmallPic />{" "}
+                      </p>
+                      <p>
+                        Films:
+                        <div className="row">
+                          {detail.films?.map((item, index) => {
+                            return (
+                              <div key={index} className="col">
+                                <SmallPic go={item} />
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </p>
+                    </div>
+                    <div>
+                      <p>
+                        Vehicles:{" "}
+                        <div className="row">
+                          {detail.vehicles?.map((item, index) => {
+                            return (
+                              <div key={index} className="col">
+                                <SmallPic go={item} />
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </p>
+                      <p>
+                        Starships:
+                        <div className="row">
+                          {detail.starships?.map((item, index) => {
+                            return (
+                              <div key={index} className="col">
+                                <SmallPic go={item} />
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
